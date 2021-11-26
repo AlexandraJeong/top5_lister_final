754c4e1581d096f34c47db68861163ded220233f
Alexandra Jeong <alexandra.jeong@stonybrook.edu>
2021-11-26 14:33:27 -0500

    partially styled splash screen

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -3,6 +3,20 @@ export default function SplashScreen() {
         <div id="splash-screen">
             The Top 5<br />
             Lister
+            <div id = "description-text">Utilize to create your own top 5 lists and view/respond to other's lists.</div>
+            <div id = "name-text">Alexandra Jeong</div>
+            <button
+                id="create-account-button"
+                className="splash-screen-button"
+                >Create<br/>Account</button>
+            <button
+                id="login-button"
+                className="splash-screen-button"
+                >Log In</button>
+            <button
+                id="guest-button"
+                className="splash-screen-button"
+                >Continue<br/>as Guest</button>
         </div>
     )
 }
